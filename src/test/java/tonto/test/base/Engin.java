154ea2b2a7049b9b5fc74772f24f2403635a8ff8
zxwgdft <823498927@qq.com>
2016-08-31 18:26:31 +0800

    反射修改

---
+++ src/test/java/tonto/test/base/Engin.java
@@ -0,0 +1,20 @@
+package tonto.test.base;
+
+/**
+ * 发动机
+ * @author TontoZhou
+ *
+ */
+public class Engin {
+	
+	private Integer power;
+
+	public Integer getPower() {
+		return power;
+	}
+
+	public void setPower(Integer power) {
+		this.power = power;
+	}
+
+}
